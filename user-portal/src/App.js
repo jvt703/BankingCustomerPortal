708e76be354d060280cafa65d339d320617b331a
John <jvt703@gmail.com>
2023-03-01 10:23:41 -0500

    N1T-261 styling updates

--- user-portal/src/App.js
+++ user-portal/src/App.js
@@ -1,15 +1,15 @@
 
 import './css/App.css';
 import HeaderComponent from './Components/Headercomponent';
-
 function App() {
   return (
     <div className="App">
       
       
       <body>
-
+        
         <HeaderComponent pageName="Accounts" pageDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"/>
+  
       </body>
     </div>
   );
--- user-portal/src/Components/Headercomponent.js
+++ user-portal/src/Components/Headercomponent.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const HeaderComponent = ({pageName, pageDescription})=>{
     return (
-        <header className="pb-2  Header ">
-        <div className="p-2 mb-2 banner-container">
+        <header className=" Header ">
+        <div className="banner-container">
           <h1 className="Header-banner ">{pageName}</h1>
           <h4 className="Header-banner-description">{pageDescription}</h4>
         </div>
