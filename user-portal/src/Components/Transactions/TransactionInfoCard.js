460c5558aa890f57a6ead14f7bc5e88ac3c1b28c
Richard <richardfolorunso@protonmail.com>
2023-03-08 23:26:35 -0500

    Backend communication

--- user-portal/src/Components/Transactions/TransactionInfoCard.js
+++ user-portal/src/Components/Transactions/TransactionInfoCard.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 
 
-const TransactionInfoCard = ({type, amount, date, points}) => {
-    
+const TransactionInfoCard = ({sourceAccount, destinationAccount, amount, date, points}) => {
+    const accountID = 1;
+    const type = (sourceAccount === accountID ? "Deposit" : (destinationAccount === accountID ? "Widthdrawl" : "Transfer"));
     return (
         <div className="card">
             <div className="card-body">
                 <div className="card-row">
                     <span>
                         <h4>{type}</h4>
-                        <h4>Amount: {amount}</h4>
+                        <h4>Amount: ${amount}</h4>
                         <h4>Date: {new Date(date).toString()}</h4>
                         <h4>Points: {points}</h4>
                     </span>
