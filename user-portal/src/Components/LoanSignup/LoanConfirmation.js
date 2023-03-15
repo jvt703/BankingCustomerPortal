5f5f1e3469ec48c699964a60bdc776eac4b6b7d7
Blaze Vincent <blaze.vincent@smoothstack.com>
2023-03-15 13:35:36 -0500

    Overhaul loan signup

--- user-portal/src/Components/LoanSignup/LoanConfirmation.js
+++ user-portal/src/Components/LoanSignup/LoanConfirmation.js
@@ -1,17 +1,10 @@
 export default function LoanConfirmation({loanResponse = {
-  accountId: Number(),
-  approvedAmount: Number(),
-  interestRate: Number(),
-  minimumPaymentAmount: Number()
+  message: String()
 }}){
-  return <div className="card" style={{width: "18rem"}}>
-    <h5 className="card-header alert alert-success">You have been granted a loan</h5>
-
-    <div className="p-4 pt-0 d-flex flex-column align-items-start">
-      <p className="card-text">Approved amount - {loanResponse.approvedAmount}</p>
-      <p className="card-text">Interest rate - {loanResponse.interestRate}</p>
-      <p className="card-text">Minimum payment amount - {loanResponse.minimumPaymentAmount}</p>
+  return <div className="container d-flex justify-content-center p-4">
+    <div className="card" style={{width: "18rem"}}>
+      <h5 className="card-header alert alert-success">Success</h5>
+      <p className="card-text pb-3">{loanResponse.message}</p>
     </div>
-
   </div>
 }
--- user-portal/src/Components/LoanSignup/LoanSignup.js
+++ user-portal/src/Components/LoanSignup/LoanSignup.js
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react"
+import ApplicationForm from "../AbstractSignupForm/ApplicationForm"
+import LabeledDropdown from "../AbstractSignupForm/LabeledDropdown"
+import LabeledInput from "../AbstractSignupForm/LabeledInput"
 import HeaderComponent from "../Headercomponent"
 import LoanConfirmation from "./LoanConfirmation"
 
@@ -7,78 +10,41 @@ export default function LoanSignup(){
   const [loanResponse, setLoanResponse] = useState(null)
   const [depositableAccounts, setDepositableAccounts] = useState([])
 
-  const [targetAccount, setTargetAccount] = useState(0)
-  const [requestedAmount, setRequestedAmount] = useState(0.0)
-
   useEffect(() => {
     fetch(process.env.REACT_APP_BACKEND_URL + "user/1/accounts")
     .then(res => res.json())
-    .then(setDepositableAccounts)
+    .then(res => setDepositableAccounts(res.filter(account => !["Loan", "Credit"].includes(account.accountTypeName))))
   }, [])
 
-  function submitRequest(){
-    if(targetAccount && requestedAmount){
-      fetch(process.env.REACT_APP_BACKEND_URL + "user/1/loan", {
-        method: "POST",
-        headers: {
-          "content-type": "application/json"
-        },
-        body: JSON.stringify({
-          loanAmount: requestedAmount,
-          debitedAccountId: targetAccount
-        })
-      })
-      .then(res => res.json())
-      .then(setLoanResponse)
-    }
-  }
-
-  return <div>
+  return <>
     <HeaderComponent pageName={"Apply for a loan"}/>
 
-    <div className="d-flex align-items-center flex-column justify-items-center p-4">
     {loanResponse 
-    ? <LoanConfirmation loanResponse={loanResponse} />
-    : <div className="card" style={{width: "18rem"}}>
-      <div className="card-body d-flex flex-column gap-2 ">
-        <h5 className="card-title">Loan details</h5>
-        <label htmlFor="loanAmount">Amount requested:</label>
-        <input 
-          onChange={e => {
-            setRequestedAmount(parseFloat(e.target.value))
-          }}
-          id="loanAmount" 
-          type="text" 
-          placeholder="1000"
+      ? <LoanConfirmation loanResponse={loanResponse} />
+      : <ApplicationForm
+        route="user/1/loanApplication"
+        handleResponse={setLoanResponse}
+      >
+        <h5>Loan details</h5>
+        <LabeledInput 
+          className="col-6"
+          inputId="loanAmount"
+          labelText="Amount requested:"
+          title="Input should be a whole number"
+          pattern="\d*"
+          useName
         />
-        <label htmlFor="accountCredited">Account to be credited:</label>
-
-        <select 
-          name="accountCredited" 
-          id="accountCredited"
-          onChange={e => {
-            setTargetAccount(e.target.value)
-          }}
-        >
-            <option value="">--Please choose an account--</option>
-            {
-              depositableAccounts
-              .filter(account => account.accountTypeName !== "Loan")
-              .map((account, index) => <option key={index} value={account.id}>
-                {account.accountName}
-              </option>)
-            }
-        </select>
-
-        <button 
-          className="btn btn-primary"
-          onClick={submitRequest}
-        >Request loan</button>
-      </div>
-    </div>
+        <LabeledDropdown
+          className="col-6"
+          inputId="debitedAccountId"
+          labelText="Account to be credited:"
+          options={depositableAccounts.map(account => {
+            return {text: account.accountName, value: account.id}
+          })}
+          useName
+        />
+      </ApplicationForm>
     }
-    </div>
     
-    
-  </div>
+  </>
 }
