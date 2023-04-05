e106cb153b2631ab1cb0e83b2e1d48dd6702d1e3
Blaze Vincent <blaze.vincent@smoothstack.com>
2023-04-05 15:15:14 -0500

    Remove hard size cap on navbar

--- user-portal/src/Components/NavBar.js
+++ user-portal/src/Components/NavBar.js
@@ -26,7 +26,6 @@ const NavBar = ({pageName, pageDescription, isAuthenticated, setIsAuthenticated}
           <Navbar.Collapse id="navbarScroll">
             <Nav
               className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
               navbarScroll
             >
               <Nav.Link>
